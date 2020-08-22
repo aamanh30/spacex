3eeef864a60859605035cc9df0a2d25a28755d89
Aaman <aaman.hussain@impelsys.com>
2020-08-22 22:00:36 +0530

    Updated the AppModule with necessary imports and routing file changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FullpageLayoutComponent, SidebarLayoutComponent } from './shared/components/';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'launch-programs',
+    component: SidebarLayoutComponent,
+    loadChildren: () => import('./features/launch-programs/launch-programs.module').then(m => m.LaunchProgramsModule)
+  },
+  {
+    path: 'error',
+    component: FullpageLayoutComponent,
+    loadChildren: () => import('./features/error/error.module').then(m => m.ErrorModule)
+  },
+  {
+    path: '',
+    redirectTo: 'launch-programs',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'error/404',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
